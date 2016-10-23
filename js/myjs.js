92b163568949b873be7743d25328cad2815fc89f
Richard.Wang <leix.wang@outlook.com>
2016-10-23 21:23:58 +0800

    pic to 12 for masonry.
    masonry.desandro.com

---
+++ js/myjs.js
@@ -0,0 +1,15 @@
+$(function(){
+  $("i.icon").addClass('fa-3x');
+  $("div.class-body").addClass('text-justify');
+  $('div.hreview').addClass('col-md-3');
+  $('div.hreview').addClass('col-sm-4');
+  $('div.hreview').addClass('col-xs-6');
+  $('img').addClass('img-responsive');
+  $('div.row div.col-md-3').addClass('thumbnail');
+
+  // masonry.desandro.com
+  $('div.review').masonry({
+    itemSelector: '.hreview'
+  })
+
+})
